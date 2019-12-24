5736fb512773f5d3844f2ac08636746d3c645b06
Yevheniia Lebid <54360187+Yevheniia-Lebid@users.noreply.github.com>
2019-12-24 23:49:01 +0200

    Add files via upload

---
+++ HomeWork1/src/com/qa/homework1/HomeWork1.java
@@ -0,0 +1,13 @@
+package com.qa.homework1;
+
+import java.util.Scanner;
+
+public class HomeWork1 {
+    public static void main(String[] arg){
+        System.out.println("* * * *" + "  * * * *" + "     *       " + "*         *");
+        System.out.println("*      " + "  *     *" + "   *   *     " + "*  *   *  *");
+        System.out.println("* *    " + "  * * * *" + "  *     *    " + "*    *    *");
+        System.out.println("*      " + "  *      " + " * * * * *   " + "*         *");
+        System.out.println("* * * *" + "  *      " + "*         *  " + "*         *");
+    }
+}
